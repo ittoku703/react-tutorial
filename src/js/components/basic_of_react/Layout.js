f8da95cf6804f114b3cf99dc079a595d9cf34340
shinzanmono <shinzanmono1192@gmail.com>
2021-08-12 16:50:06 +0900

    moved files to basic_of_react

--- src/js/components/basic_of_react/Layout.js
+++ src/js/components/basic_of_react/Layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Header from "./Header"
-import Footer from "./Footer"
 
 export default class Layout extends React.Component {
   constructor() {
@@ -14,7 +13,6 @@ export default class Layout extends React.Component {
     return (
       <div>
         <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
       </div>
     );
   }
