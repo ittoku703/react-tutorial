837f294dbda7224f70276084337fb84d78affbc2
shinzanmono <shinzanmono1192@gmail.com>
2021-08-16 01:01:00 +0900

    created template for react router

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Layout from './components/Layout'
 
+import BasicOfLayout from './components/basic_of_react/Layout'
+import ReactRouterLayout from './components/react_router/Layout'
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+
+// basic of react
+// ReactDOM.render(<BasicOfLayout/>, app);
+// react router
+ReactDOM.render(<ReactRouterLayout />, app);
+
