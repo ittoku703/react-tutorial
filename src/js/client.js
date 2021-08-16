24a2c432d12bdd20cb22f675a8fc9ca4df1a9b98
shinzanmono <shinzanmono1192@gmail.com>
2021-08-17 01:34:25 +0900

    reset tutorial

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import BasicOfLayout from './components/basic_of_react/Layout'
-import ReactRouterLayout from './components/react_router/Layout'
-
 const app = document.getElementById('app');
-
-// basic of react
-// ReactDOM.render(<BasicOfLayout/>, app);
-// react router
-ReactDOM.render(<ReactRouterLayout />, app);
-
+ReactDOM.render(<h1>Welcome</h1>, app);
