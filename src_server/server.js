a2363d8d97595366f4081295e891d101dd886130
shinzanmono <shinzanmono1192@gmail.com>
2021-09-13 14:18:02 +0900

    add about routing

--- src_server/server.js
+++ src_server/server.js
@@ -3,6 +3,7 @@ import webpack from 'webpack'
 import WebpackDevMiddleware from 'webpack-dev-middleware'
 import WebpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../webpack.config.js'
+import path from 'path'
 
 const app = express()
 const port = 8080
@@ -20,7 +21,18 @@ if (devServerEnables) {
 
 app.use(express.static('./public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(pathToHtml('index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(pathToHtml('about.html'))
+})
 
 app.listen(port, () => {
   console.log(`App listening to ${port}`)
 })
+
+function pathToHtml(filename = "") {
+  return path.join(__dirname, '../', 'public', filename)
+}
